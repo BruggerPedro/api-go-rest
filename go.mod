6d5f68db693fe7a301ae1005516070bc9ba1612c
Pedro Brügger <pedromurilobrugger@hotmail.com>
2023-01-20 12:52:42 -0300

    Iniciando API Rest

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/BruggerPedro/api-go-rest
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/BruggerPedro/api-go-rest/controllers"
+)
+
+func HandleRequest() {
+	http.HandleFunc("/", controllers.Home)
+	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
